01f1aa3db79d005a3741bdf5dbe1e48c54fd1210
Serhii Chernenko <contact@chernenko.digital>
2025-06-08 11:48:23 +0200

    fix(eslint): config updated

---
+++ eslint.config.mjs
@@ -0,0 +1,71 @@
+import stylistic from '@stylistic/eslint-plugin'
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+const generalConfig = {
+  name: '@courses/eslint-rules',
+  plugins: {
+    '@stylistic': stylistic,
+  },
+  files: ['**/*.ts', '**/*.mts', '**/*.cts', '**/*.js', '**/*.mjs', '**/*.cjs', '**/*.vue'],
+  rules: {
+    'no-console': 'warn',
+    'unicorn/prefer-number-properties': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'vue/no-v-html': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-invalid-void-type': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
+    '@typescript-eslint/no-unsafe-function-type': 'off',
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        ignoreRestSiblings: false,
+        reportUsedIgnorePattern: false,
+      },
+    ],
+    '@stylistic/indent': ['error', 2],
+    '@stylistic/brace-style': ['error', '1tbs'],
+    '@stylistic/max-len': [
+      'error',
+      {
+        code: 80,
+        ignorePattern: '^(import\\s.+\\sfrom\\s.+|\\} from)|.*class=".*"',
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+}
+
+const stylisticCustomConfig = {
+  indent: 2,
+  semi: false,
+  quotes: 'single',
+}
+
+const stylisticResultConfig = stylistic.configs.customize(stylisticCustomConfig)
+
+export const configNuxt = [stylisticResultConfig, generalConfig]
+
+export const configStandalone = createConfigForNuxt({
+  features: {
+    tooling: true,
+    stylistic: stylisticCustomConfig,
+  },
+}).append(stylisticResultConfig, generalConfig)
+
+export default withNuxt(configNuxt)
