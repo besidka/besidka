53d5c19123b27487d3fa14a5b52a0cfa087fb76e
Serhii Chernenko <contact@chernenko.digital>
2025-06-19 08:01:01 +0200

    fix(auth layout): bottom padding added to avoid main nav overlapping

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid place-items-center w-full min-h-svh p-4">
-    <div class="grid place-items-center w-full">
+    <div class="grid place-items-center w-full pb-20 sm:pb-10">
       <div class="grid place-items-center mb-8">
         <Logo class="min-h-auto !m-0" />
       </div>
