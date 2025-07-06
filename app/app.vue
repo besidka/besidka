2e892945bc9875eb6349a4bf11afd43b7eb053b5
Serhii Chernenko <contact@chernenko.digital>
2025-07-06 23:27:11 +0200

    docs(cloneathon): domain and branch removed, readme updated after hackathon result announced

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
   <NuxtRouteAnnouncer />
-  <LazyUiAlert
-    v-if="!isAlertHidden"
-    @click="isAlertHidden = true"
-  >
-    <NuxtLink to="https://cloneathon.t3.chat" external target="_blank"><strong>T3 Cloneathon</strong></NuxtLink>: The code freeze version, prepared for the hackathon deadline, is available at <NuxtLink to="https://cloneathon.chernenko.chat" external rel="">https://cloneathon.chernenko.chat</NuxtLink>.
-  </LazyUiAlert>
   <NuxtErrorBoundary @error="onException">
     <template #error>
       <NuxtLayout />
@@ -26,10 +20,6 @@ useHead({
   titleTemplate(titleChunk) {
     return titleChunk ? `${titleChunk} | ${siteName}` : `${siteName}`
   },
-})
-
-const isAlertHidden = useCookie('alert-hidden', {
-  default: () => false,
 })
 
 function onException(exception: any) {
