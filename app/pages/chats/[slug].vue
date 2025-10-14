a6820fe10b35d12dc8dd59b70171a607c8d81d8e
Serhii Chernenko <contact@chernenko.digital>
2025-10-14 11:44:17 +0200

    style(chat input): user icon without image adjusted for dark theme

--- app/pages/chats/[slug].vue
+++ app/pages/chats/[slug].vue
@@ -27,11 +27,11 @@
                 'max-sm:hidden': m.role === 'assistant',
             }"
           >
-            <div class="w-10 rounded-full bg-base-100 dark:bg-base-content">
+            <div class="w-10 rounded-full bg-base-100 dark:bg-base-content text-text dark:text-base-100">
               <Logo
                 v-if="m.role === 'assistant'"
                 short
-                class="size-6 text-text dark:text-base-100"
+                class="size-6"
               />
               <template v-else>
                 <img
