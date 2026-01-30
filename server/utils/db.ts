aad78ae9dd9a7119be260a1d71aff208fd3334f0
Pooya Parsa <pyapar@gmail.com>
2026-01-30 16:03:47 +0100

    fix: fix cloudflare related issues (#57)
    * fix: fix cloudflare related issues
    
    * fix: ts ignore added
    
    ---------
    
    Co-authored-by: Serhii Chernenko <contact@chernenko.digital>

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,17 +1,12 @@
-import type { H3Event } from 'h3'
 import type { D1Database } from '@cloudflare/workers-types'
+// @ts-ignore
+import { env } from 'cloudflare:workers'
 import { drizzle } from 'drizzle-orm/d1'
 import { EnhancedQueryLogger } from 'drizzle-query-logger'
-import * as schema from '../db/schema'
+import * as schema from '~~/server/db/schema'
 
-export function useDb(event?: H3Event) {
-  event = event ?? useEvent()
-
-  if (!event.context?.cloudflare?.env) {
-    throw createError('Cloudflare environment not found in event context')
-  }
-
-  const db = event.context.cloudflare.env.DB
+export function useDb() {
+  const db = env.DB
 
   if (!db) {
     throw createError('Database not found in ENV: DB')
--- server/utils/kv.ts
+++ server/utils/kv.ts
@@ -1,14 +1,9 @@
-import type { H3Event } from 'h3'
 import type { KVNamespace } from '@cloudflare/workers-types'
+// @ts-ignore
+import { env } from 'cloudflare:workers'
 
-export function useKV(event?: H3Event) {
-  event = event ?? useEvent()
-
-  if (!event.context?.cloudflare?.env) {
-    throw createError('Cloudflare environment not found in event context')
-  }
-
-  const { KV } = event.context.cloudflare.env
+export function useKV() {
+  const { KV } = env
 
   if (!KV) {
     throw createError(`KV not found in ENV: KV`)
